1ff5bac1fa4308e5b427f599dee0704e89035c91
Cristian Garcia <criscapo0901@gmail.com>
2020-12-04 17:04:50 -0300

    Creacion del registro para las mascotas

--- Recepcion.cpp
+++ Recepcion.cpp
@@ -3,11 +3,37 @@
 #include <string.h>
 
 
+typedef char palabra[60];
+
 int menuPrincipal();
+
+
+
+
+
+struct fecha
+{
+	int dia;
+	int mes;
+	int anio;
+};
+
+struct registro
+{
+	palabra ApeYNom;
+	palabra Domicilio;
+	int DNI;
+	palabra Localidad;
+	float Peso;
+	int Telefono;
+	fecha fec;
+};
 
 main()
 {
 	int opc=0;
+	registro mascota; //Declarar registro de mascotas
+	FILE*arch;//Variable puntero de mascotas
 	
 	do{
 		opc = menuPrincipal();
@@ -16,7 +42,54 @@ main()
 			case 1: 
 		    	break;
 			case 2:
+			{
+				system("CLS");
+				arch = fopen("Mascotas.dat","r+b");
+				if(arch == NULL)
+				{
+					fclose(arch);
+					arch= fopen("Mascotas.dat","w+b");
+				}
+				
+				printf("-Registrar mascota-\n\n");
+				_flushall();
+				
+				printf("Apellido y nombre: ");
+				gets(mascota.ApeYNom);
+				
+				printf("\nDomicilio: ");
+				gets(mascota.Domicilio);
+				
+				printf("\nDNI del duenio: ");
+				scanf("%d", &mascota.DNI);
+				_flushall();
+				
+				
+				printf("\nLocalidad: ");
+				gets(mascota.Localidad);
+				
+				
+				printf("\nPeso de la mascota: ");
+				scanf("%f", &mascota.Peso);
+				
+				printf("\nTelefono: ");
+				scanf("%d", &mascota.Telefono);
+				
+				printf("\n*Fecha de Nacimiento*");
+				printf("\n\nDia: ");
+				scanf("%2d", &mascota.fec.dia);
+				printf("\nMes: ");
+				scanf("%2d",  &mascota.fec.mes);
+				printf("\nAnio: ");
+				scanf("%4d", &mascota.fec.anio);
+				
+				fwrite(&mascota, sizeof (registro), 1, arch );
+				
+				fclose(arch);
+				
+				system("PAUSE");	
 				break;
+			}
 			case 3:
 				break;
 			case 4:

a63c9a8d6739cf0f0b9cb320ac94f7cb45cde623
Fran14 <francogarbero@hotmail>
2020-12-08 14:35:31 -0300

    Inicio desarrollo de funciones principales para administracion

--- Admin.cpp
+++ Admin.cpp
@@ -1,35 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct usuario{
+	char user[10];
+	char contra[10];
+	char apynom[60];
+};
+
+struct veterinario{
+	char apynom[60];
+	int matricula;
+	int dni;
+	int telefono;
+	char contra[10];
+};
 
 int menuPrincipal();
-void registroVet();
-void registroAsist();
+void registroVet(FILE *archVet);
+void registroAsist(FILE *archUs);
 int atenciones();
 int ranking();
 
 main()
 {
+	FILE *archUs = fopen("Usuarios.dat","r+b");
+	if(archUs == NULL){
+		fclose(archUs);
+		archUs = fopen("Usuarios.dat","a+b")
+	}
+	
+	FILE *archVet = fopen("Veterinarios.dat","r+b");
+	if(archVet == NULL){
+		fclose(archVet);
+		archVet = fopen("Veterinarios.dat","a+b")
+	}
+		
 	int opc=0;
 	
 	do{
 		opc= menuPrincipal();
 		switch(opc){
 			case 1:
-				
+			{				
+				registroVet(archVet);
 				break;
+			}
 			case 2:
 			{
-			
+				registroAsist(archUs);				
 				break;
 			}
 			case 3:
 			{
-			
+				atenciones();
 				break;
 			}	
 			case 4:
 			{
-	
+				ranking();
 				break;
 			}
 			
@@ -58,14 +87,42 @@ int menuPrincipal()
 	return op;
 }
 
-void registroVet()
+void registroVet(FILE *archVet)
 {
+	veterinario regVet;
+	
+	_flushall();
+	system("CLS");
+	printf("\n\n\t\t");
+	printf("\n\n\t\t           INICIO REGISTRO VETERINARIO                ");
+	printf("\n\n\t\t======================================================");
+	printf("\n\n\t\t Ingrese Apellido y nombre: ");
+	gets(regVet.apynom);
+	printf("\n\n\t\t Matricula: ");
+	scanf("%d",&regVet.matricula);
+	printf("\n\n\t\t DNI: ");
+	scanf("%d",&regVet.dni);
+	printf("\n\n\t\t Telefono: ");
+	scanf("%d",&regVet.telefono);
+	printf("\n\n\t\t Contrasenia para ingreso: ");
+	
 	
 }
 
-void registroAsist()
+void registroAsist(FILE *archUs)
 {
+	usuario regUs;
 	
+	_flushall();
+	system("CLS");
+	printf("\n\n\t\t");
+	printf("\n\n\t\t           INICIO REGISTRO ASISTENTE                  ");
+	printf("\n\n\t\t======================================================");
+	printf("\n\n\t\t Ingrese Apellido y nombre: ");
+	gets(regUs.apynom);
+	printf("\n\n\t\t Usuario para ingreso: ");
+
+	printf("\n\n\t\t Contrasenia para ingreso: ");
 }
 
 int atenciones()

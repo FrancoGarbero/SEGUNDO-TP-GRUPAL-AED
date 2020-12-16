d2ca6603685a59bf867bc76d40205b0287326629
Fran14 <francogarbero@hotmail>
2020-12-16 11:29:48 -0300

    Se agregaron funciones al menu principal para mejorar la estetica del ejecutable

--- Menu_Principal.cpp
+++ Menu_Principal.cpp
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include <windows.h>
 
 int menuPrincipal();
+void gotoxy(int x,int y);
+void cuadro(int x1, int y1, int x2, int y2);
 
 main()
 {
+	system("color B0");
+	
 	int opc=0;
 		do{
 		opc = menuPrincipal();
@@ -48,16 +53,49 @@ main()
 int menuPrincipal(){
 	int op;
 	system("CLS");
-	printf("\n\n\t\t");
-	printf("\n\n\t\t|------------------------------------------------------|");
-	printf("\n\n\t\t|          		V E T E R I N A R I A		           |");
-	printf("\n\n\t\t|------------------------------------------------------|");
-	printf("\n\n\t\t|     1 - Consultorio					               |");
-	printf("\n\n\t\t|     2 - Recepcion									   |");
-	printf("\n\n\t\t|     3 - Administracion						       |");
-	printf("\n\n\t\t|     4 - Salir						                   |");
-	printf("\n\n\t\t|------------------------------------------------------|");
+	
+	cuadro(1,1,77,3);
+	gotoxy (30,2);
+	printf("V E T E R I N A R I A\n\n");
+	printf("\n\n\t\t|     1 - Consultorio");
+	printf("\n\n\t\t|     2 - Recepcion");
+	printf("\n\n\t\t|     3 - Administracion");
+	printf("\n\n\t\t|     4 - Salir");
+
 	printf("\n\n\t\t     Seleccione una Opcion: "); 
 	scanf("%d", &op);
 	return op;
+}
+
+/*------------------------------------*/
+void gotoxy(int x,int y)
+{  
+	HANDLE hcon;  
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+    COORD dwPos;  
+    dwPos.X = x;  
+    dwPos.Y= y;  
+    SetConsoleCursorPosition(hcon,dwPos);  
+}
+
+/*------------------------------------*/
+void cuadro(int x1, int y1, int x2, int y2)
+{
+	int i;
+    for (i=x1;i<x2;i++)
+	{	
+		gotoxy(i,y1);printf("Ä") ; //linea horizontal superior 
+		gotoxy(i,y2);printf("Ä") ; //linea horizontal inferior
+    }
+
+    for (i=y1;i<y2;i++)
+	{	
+		gotoxy(x1,i);printf("³") ; //linea vertical izquierda
+		gotoxy(x2,i);printf("³") ; //linea vertical derecha
+    }
+    
+    gotoxy(x1,y1); printf("Ú"); //Esquinas del cuadro
+    gotoxy(x1,y2); printf("À");
+    gotoxy(x2,y1); printf("¿");
+    gotoxy(x2,y2); printf("Ù");
 }
